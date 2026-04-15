b6089d32346606980b0b9745d21de69b9c6a2e24
skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
2026-04-14 21:01:25 -0700

    Roll Dawn from d0a283a7a5e6 to 63b237e40fc2 (10 revisions)
    https://dawn.googlesource.com/dawn.git/+log/d0a283a7a5e6..63b237e40fc2
    
    2026-04-15 lokokung@google.com [dawn] Make sure to wait for callbacks before setting logging callback.
    2026-04-15 chouinard@google.com [tint][hlsl] Only polyfill trunc on FXC
    2026-04-15 bsheedy@google.com Remove dead legacy_builders code
    2026-04-15 shrekshao@google.com WebGPU Backend: Add EnableTintIRValidationAsserts to kOuterToggles
    2026-04-15 bsheedy@google.com Move presubmit trybot out of legacy_builders.star
    2026-04-14 bajones@chromium.org Vulkan: Avoid potential double-free of VkCommandPool
    2026-04-14 bsheedy@google.com Add Win/AMD RX 5500 XT to CQ
    2026-04-14 rharrison@chromium.org Migrate to using tools/code_coverage/coverage.py
    2026-04-14 amaiorano@google.com Add Gemini context files
    2026-04-14 dawn-autoroll@skia-public.iam.gserviceaccount.com Roll ANGLE from fd958ac6533e to 5e591d03650d (6 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/dawn-skia-autoroll
    Please CC amaiorano@google.com,cwallez@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Dawn: https://bugs.chromium.org/p/dawn/issues/entry
    To file a bug in Skia: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Cq-Include-Trybots: skia/skia.primary:Build-Ubuntu24.04-Clang-x86_64-Debug-Graphite_Dawn_Vulkan;skia/skia.primary:Test-Mac14-Clang-MacMini9.1-GPU-AppleM1-arm64-Debug-All-ASAN_Graphite_Dawn_Metal;skia/skia.primary:Test-Win11-MSVC-Golo-GPU-QuadroP400-x86_64-Release-All-Graphite_Dawn_D3D12;skia/skia.primary:Test-Win11-MSVC-Golo-GPU-QuadroP400-x86_64-Release-All-Graphite_Dawn_D3D11
    Bug: None
    Tbr: amaiorano@google.com
    Change-Id: I320b8e0bd45c9f3d379cb7de593c8d83d30c7804
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1210377
    Commit-Queue: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- infra/bots/deps/deps_gen.go
+++ infra/bots/deps/deps_gen.go
@@ -164,7 +164,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"dawn.googlesource.com/dawn": {
 		Id:      "dawn.googlesource.com/dawn",
-		Version: "d0a283a7a5e6320ca919f9580590371086f41dd6",
+		Version: "63b237e40fc26a0014e059221393ab43833af99f",
 		Path:    "third_party/externals/dawn",
 	},
 	"infra/3pp/tools/ninja": {
